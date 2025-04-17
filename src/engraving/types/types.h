68c529c286805869622ca7b63c78e4c0d3a798f5
Nathan Reed <nathaniel.reed@gmail.com>
2025-04-16 22:26:15 -0700

    Add option for "invert score" to follow app theme

--- src/engraving/types/types.h
+++ src/engraving/types/types.h
@@ -1209,6 +1209,16 @@ enum class LayoutFlag : unsigned char {
 };
 
 typedef muse::Flags<LayoutFlag> LayoutFlags;
+
+//---------------------------------------------------------
+//  ScoreInversionMode
+//---------------------------------------------------------
+
+enum class ScoreInversionMode : unsigned char {
+    Disabled,
+    FollowAppTheme,
+    Always
+};
 } // mu::engraving
 
 template<>
