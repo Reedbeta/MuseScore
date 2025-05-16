0106e26d1340618f5b1f0b0cf7772fc5adaf5310
Nathan Reed <nathaniel.reed@gmail.com>
2025-05-15 20:24:45 -0700

    Add select notes by chord index

--- src/notation/view/widgets/selectnotedialog.cpp
+++ src/notation/view/widgets/selectnotedialog.cpp
@@ -160,6 +160,10 @@ FilterNotesOptions SelectNoteDialog::noteOptions() const
         options.system = m_note->chord()->segment()->system();
     }
 
+    if (sameChordVoice->isChecked()) {
+        options.chordIndex = m_note->chordIndex();
+    }
+
     return options;
 }
 
