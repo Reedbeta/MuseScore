bb6d429a63aebfe8e16b589baca81363b54c8bc3
Nathan Reed <nathan@reedbeta.com>
2026-01-01 18:29:22 -0800

    Fix changeSelectedElementsVoice in local time signature

--- src/engraving/dom/score.cpp
+++ src/engraving/dom/score.cpp
@@ -5401,7 +5401,7 @@ void Score::changeSelectedElementsVoice(voice_idx_t voice)
                 if (voice && !dstCR) {
                     score->expandVoice(s, /*m->first(SegmentType::ChordRest,*/ dstTrack);
                 }
-                score->makeGapVoice(s, dstTrack, chord->actualTicks(), s->tick());
+                score->makeGapVoice(s, dstTrack, chord->ticks(), s->tick());
             }
 
             if (!dstChord) {
